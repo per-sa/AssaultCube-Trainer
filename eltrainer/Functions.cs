e81aa4d964518d622dccaf39a0243b369bb7afa8
matheusclmb <ccolombomatheus@gmail.com>
2022-12-28 01:51:44 +0000

    Feat: Added Offsets and Classes

---
+++ eltrainer/Functions.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using swed32;
+
+namespace eltrainer
+{
+    public class methods
+    {
+        public swed mem;
+        public IntPtr moduleBase;
+
+        public Entity ReadLocalPLayer()
+        {
+            var localPlayer = ReadEntity(mem.ReadPointer(moduleBase, Offsets.iLocalPlayer));
+            localPlayer.viewAngles.X = mem.ReadFloat(localPlayer.baseAddress, Offsets.vAngles);
+            localPlayer.viewAngles.Y = mem.ReadFloat(localPlayer.baseAddress, Offsets.vAngles + 0x4);
+            return localPlayer;
+        }
+
+        public Entity ReadEntity(IntPtr entBase)
+        {
+            var ent = new Entity();
+
+            ent.baseAddress = entBase;
+
+            ent.currentAmmo = mem.ReadInt(ent.baseAddress, Offsets.iCurrentAmmo);
+            ent.health = mem.ReadInt(ent.baseAddress, Offsets.iHealth);
+            ent.team = mem.ReadInt(ent.baseAddress, Offsets.iTeam);
+
+            ent.feet = mem.ReadVector3(entBase, Offsets.vFeet);
+            ent.head = mem.ReadVector3(ent.baseAddress, Offsets.vHead);
+
+            ent.name = Encoding.UTF8.GetString(mem.ReadBytes(ent.baseAddress, Offsets.sName, 11));
+
+            return ent;
+        }
+
+
+        public methods()
+        {
+            mem = new swed();
+            mem.GetProcess("ac_client");
+            moduleBase = mem.GetModuleBase(".exe");
+
+        }
+    }
+}
---
+++ eltrainer/Offsets.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eltrainer
+{
+    public class Offsets
+    {
+        public static int
+            iViewMatrix = 0x17DFFC,
+            iLocalPlayer = 0x0018AC00,
+            iEntityList = 0x00191FCC,
+
+            // offsets from entities
+
+            vHead = 0x4,
+            vFeet = 0x30,
+            vAngles = 0x34,
+            iHealth = 0xEC,
+            iDead = 0xB4,
+            sName = 0x205,
+            iTeam = 0x30C,
+            iCurrentAmmo
+            ;
+    }
+}
